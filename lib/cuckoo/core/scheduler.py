804ccf0fabc72600b9292d49919e0ee9b8223c61
Thorsten Sick <thorsten.sick@avira.com>
2014-03-26 11:11:32 +0100

    Adding a machine ID to the task. This way we can identify broken VMs that do not generate proper output
    (cherry picked from commit 7972f4738a2301b567a308d224d3fbf0c46006d8) from Thorsten Sick
    
    Conflicts:
    	lib/cuckoo/core/database.py
    	lib/cuckoo/core/scheduler.py

--- lib/cuckoo/core/scheduler.py
+++ lib/cuckoo/core/scheduler.py
@@ -233,7 +233,8 @@ class AnalysisManager(Thread):
 
         # At this point we can tell the ResultServer about it.
         try:
-            ResultServer().add_task(self.task, self.machine)
+            Database().recruit_machine(self.task.id, self.machine.id)
+            Resultserver().add_task(self.task, self.machine)
         except Exception as e:
             machinery.release(self.machine.label)
             self.errors.put(e)
