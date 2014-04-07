3884a287b78d3fa688ddb4a1b90db764b1293845
Will Metcalf <william.metcalf@gmail.com>
2014-04-07 16:03:22 -0500

    Update VT With URL Scrub and File/URL Disable toggles
    Signed-off-by: Will Metcalf <william.metcalf@gmail.com>
    (cherry picked from commit 9192fc86ec3ec355e4c90a386ad4a97ae344051e)
    
    Conflicts:
    	modules/processing/virustotal.py

--- modules/processing/virustotal.py
+++ modules/processing/virustotal.py
@@ -6,6 +6,7 @@ import os
 import json
 import urllib
 import urllib2
+import re
 
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.common.exceptions import CuckooProcessingError
@@ -16,6 +17,15 @@ VIRUSTOTAL_URL_URL = "https://www.virustotal.com/vtapi/v2/url/report"
 
 class VirusTotal(Processing):
     """Gets antivirus signatures from VirusTotal.com"""
+    def getbool(self,s):
+        if isinstance(s,bool):
+            rtn = s
+        else:
+            try:
+                rtn = s.lower() in ("yes", "true", "1")
+            except:
+                rtn = False
+        return rtn
 
     def run(self):
         """Runs VirusTotal processing
@@ -26,22 +36,39 @@ class VirusTotal(Processing):
 
         key = self.options.get("key", None)
         timeout = self.options.get("timeout", 60)
+        urlscrub = self.options.get("urlscrub", None)
+        do_file_lookup = self.getbool(self.options.get("do_file_lookup", False))
+        do_url_lookup = self.getbool(self.options.get("do_url_lookup", False))
 
         if not key:
             raise CuckooProcessingError("VirusTotal API key not "
                                         "configured, skip")
 
-        if self.task["category"] == "file":
+        if self.task["category"] == "file" and do_file_lookup:
             if not os.path.exists(self.file_path):
                 raise CuckooProcessingError("File {0} not found, skipping it".format(self.file_path))
 
             resource = File(self.file_path).get_md5()
             url = VIRUSTOTAL_FILE_URL
-        elif self.task["category"] == "url":
+
+        elif self.task["category"] == "url" and do_url_lookup:
             resource = self.task["target"]
+            if urlscrub:
+                urlscrub_compiled_re = None
+                try:
+                    urlscrub_compiled_re = re.compile(urlscrub)
+                except Exception as e:
+                    raise CuckooProcessingError("Failed to compile urlscrub regex" % (e))
+                try:
+                   resource = re.sub(urlscrub_compiled_re,"",resource)
+                except Exception as e:
+                    raise CuckooProcessingError("Failed to scrub url" % (e))
             url = VIRUSTOTAL_URL_URL
         else:
             # Not supported type, exit.
+            return virustotal
+
+        else:
             return virustotal
 
         data = urllib.urlencode({"resource": resource, "apikey": key})
@@ -63,7 +90,6 @@ class VirusTotal(Processing):
         except ValueError as e:
             raise CuckooProcessingError("Unable to convert response to "
                                         "JSON: {0}".format(e))
-
         if "scans" in virustotal:
             items = virustotal["scans"].items()
             virustotal["scans"] = dict((engine.replace(".", "_"), signature)
