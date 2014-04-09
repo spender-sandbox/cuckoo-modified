6f78adffef95a6c225c899517134caa2785d128f
Thorsten Sick <thorsten.sick@avira.com>
2014-04-09 13:41:35 +0200

    Adding timeout to db and statistics
    (cherry picked from commit b751f2c95e42e6e2e7e71460505d9238bf98f64c)

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -59,12 +59,14 @@ REGISTRY_KEYS_MODIFIED = "registry_keys_modified"
 CRASH_ISSUES = "crash_issues"
 ANTI_ISSUES = "anti_something_issues"
 DOTNET_ISSUES = "dotnet_issues"
+TIMEOUT = "analysis_timeout"
 
 TASK_ISSUE_NONE = "no_issue"
 TASK_ISSUE_SHORT_API_CALL_LIST = "short_api_call_list"
 TASK_ISSUE_CRASH = "crash_issue"
 TASK_ISSUE_ANTI = "anti_issue"
 TASK_ISSUE_PERFECT = "no_issue_perfect_results"
+TASK_TIMEOUT = "task_timeout"
 
 # Secondary table used in association Machine - Tag.
 machines_tags = Table(
@@ -322,6 +324,7 @@ class Task(Base):
     signatures_finished_on = Column(DateTime(timezone=False), nullable=True)
     reporting_started_on = Column(DateTime(timezone=False), nullable=True)
     reporting_finished_on = Column(DateTime(timezone=False), nullable=True)
+    timeout = Column(Boolean, nullable=False, default=False)
 
     sample_id = Column(Integer, ForeignKey("samples.id"), nullable=True)
     sample = relationship("Sample", backref="tasks")
@@ -650,6 +653,8 @@ class Database(object):
                 row.dotnet_issues = value
             elif event == REGISTRY_KEYS_MODIFIED:
                 row.registry_keys_modified = value
+            elif event == TIMEOUT:
+                row.timeout = value
 
             session.commit()
         except SQLAlchemyError as e:
@@ -1296,7 +1301,10 @@ class Database(object):
             if issue != TASK_ISSUE_NONE and issue != TASK_ISSUE_PERFECT:
                 unfiltered = unfiltered.filter(Task.signatures_alert == 0)
 
-            if issue == TASK_ISSUE_SHORT_API_CALL_LIST:
+            if issue == TASK_TIMEOUT:
+                unfiltered = unfiltered.filter(Task.timeout == True)
+
+            elif issue == TASK_ISSUE_SHORT_API_CALL_LIST:
                 unfiltered = unfiltered.filter(Task.api_calls <= api_call_limit)
 
             elif issue == TASK_ISSUE_CRASH:
