4c75ce1c0218fcc9a712ddd63b007d3328d7cf63
Brad Spengler <bspengler@accuvant.com>
2014-08-21 14:18:48 -0400

    Use a custom json decoder to filter out the newly added "raw_value" keys from reporting

--- modules/reporting/jsondump.py
+++ modules/reporting/jsondump.py
@@ -9,6 +9,18 @@ import codecs
 from lib.cuckoo.common.abstracts import Report
 from lib.cuckoo.common.exceptions import CuckooReportError
 
+class FilteredEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, dict):
+                args = obj["arguments"]
+                if args:
+                    newdict = args.copy()
+                    for arg in newdict:
+                        del arg["raw_value"]
+                    return json.JSONEncoder.default(self, newdict)
+            else:
+                return json.JSONEncoder.default(self, obj)
+
 class JsonDump(Report):
     """Saves analysis results in JSON format."""
 
@@ -20,7 +32,7 @@ class JsonDump(Report):
         try:
             path = os.path.join(self.reports_path, "report.json")
             report = codecs.open(path, "w", "utf-8")
-            json.dump(results, report, sort_keys=False, indent=4)
+            json.dump(results, report, cls=FilteredEncoder, sort_keys=False, indent=4)
             report.close()
         except (UnicodeError, TypeError, IOError) as e:
             raise CuckooReportError("Failed to generate JSON report: %s" % e)
