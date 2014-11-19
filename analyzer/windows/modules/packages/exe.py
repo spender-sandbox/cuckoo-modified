ed4be08e05a86c3b506616f072f2fd6f76b8b5f0
Brad Spengler <bspengler@accuvant.com>
2014-11-19 09:33:07 -0500

    Add new option called "appdata" to the exe analysis package, it will copy the malware to the appdata folder and launch it from there, as required for some malware to exhibit their full behavior

--- analyzer/windows/modules/packages/exe.py
+++ analyzer/windows/modules/packages/exe.py
@@ -1,6 +1,9 @@
-# Copyright (C) 2010-2014 Cuckoo Foundation.
+# Copyright (C) 2010-2014 Cuckoo Foundation, Accuvant, Inc. (bspengler@accuvant.com)
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
+
+import os
+import shutil
 
 from lib.common.abstracts import Package
 
@@ -9,4 +12,11 @@ class Exe(Package):
 
     def start(self, path):
         args = self.options.get("arguments")
+        appdata = self.options.get("appdata")
+        if appdata:
+            # run the executable from the APPDATA directory, required for some malware
+            basepath = os.getenv('APPDATA')
+            newpath = os.path.join(basepath, os.path.basename(path))
+            shutil.copy(path, newpath)
+            path = newpath
         return self.execute(path, args)
