c222a284a06e7fdf043a069ce7706775054c9a37
Brad Spengler <bspengler@accuvant.com>
2014-12-02 14:36:53 -0500

    Unpack the parentcaller properly for the resulthandler

--- lib/cuckoo/core/resultserver.py
+++ lib/cuckoo/core/resultserver.py
@@ -271,7 +271,7 @@ class ResultHandler(SocketServer.BaseRequestHandler):
             raise CuckooOperationalError("Netlog failure, call "
                                          "before process.")
 
-        apiindex, status, returnval, tid, timediff, caller = context
+        apiindex, status, returnval, tid, timediff, caller, parentcaller = context
 
         # log.debug("log_call> tid:{0} apiname:{1}".format(tid, apiname))
 
@@ -285,7 +285,7 @@ class ResultHandler(SocketServer.BaseRequestHandler):
         if self.logfd:
             print >>self.logfd, ",".join("\"{0}\"".format(i) for i in [
                 timestring, self.pid, self.procname, tid, self.ppid, caller,
-                modulename, apiname, status, returnval] + argumentstrings)
+                parentcaller, modulename, apiname, status, returnval] + argumentstrings)
 
     def log_error(self, emsg):
         log.warning("ResultServer error condition on connection %s "
