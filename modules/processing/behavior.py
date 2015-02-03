cuckoosandbox/cuckoo

Added cache to convert_to_printable. Speedup by about 10 percent, depending on number of signatures

modules/processing/behavior.py
@@ -46,6 +46,7 @@ class ParseProcessLog(list):
         self.first_seen = None
         self.calls = self
         self.lastcall = None
+        self.conversion_cache = {}
 
         if os.path.exists(log_path) and os.stat(log_path).st_size > 0:
             self.parse_first_and_reset()
@@ -194,7 +195,7 @@ class ParseProcessLog(list):
 
             argument["name"] = arg_name
 
-            argument["value"] = convert_to_printable(cleanup_value(arg_value))
+            argument["value"] = convert_to_printable(cleanup_value(arg_value), self.conversion_cache)
             arguments.append(argument)
 
         call["timestamp"] = timestamp
@@ -206,7 +207,7 @@ class ParseProcessLog(list):
         if isinstance(return_value, int):
             call["return"] = "0x%.08x" % return_value
         else:
-            call["return"] = convert_to_printable(cleanup_value(return_value))
+            call["return"] = convert_to_printable(cleanup_value(return_value), self.conversion_cache)
 
         call["arguments"] = arguments
         call["repeated"] = 0

spender-sandbox/cuckoo-modified

(cherry picked from commit 5afe8977a1e852a9d16a129d28d6bcea17fe61f3) from Thorsten Sick
Conflicts:
	modules/processing/behavior.py

modules/processing/behavior.py
@@ -41,6 +41,7 @@ class ParseProcessLog(list):
         self.lastcall = None
         self.api_count = 0
         self.call_id = 0
+        self.conversion_cache = {}
         self.cfg = Config()
         self.api_limit = self.cfg.processing.analysis_call_limit  # Limit of API calls per process
 
@@ -272,7 +273,7 @@ class ParseProcessLog(list):
 
             argument["name"] = arg_name
 
-            argument["value"] = convert_to_printable(str(arg_value))
+            argument["value"] = convert_to_printable(str(arg_value), self.conversion_cache)
             argument["raw_value"] = arg_value
             pretty = pretty_print_arg(category, api_name, arg_name, argument["value"])
             if pretty:
@@ -290,7 +291,7 @@ class ParseProcessLog(list):
         if isinstance(return_value, int) or isinstance(return_value, long):
             call["return"] = "0x%.08x" % return_value
         else:
-            call["return"] = convert_to_printable(str(return_value))
+            call["return"] = convert_to_printable(str(return_value), self.conversion_cache)
 
         prettyret = pretty_print_retval(category, api_name, call["status"], call["return"])
         if prettyret: