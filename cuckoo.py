cd355de6be0b245956e8b26742e0b08d3b13f09a
Brad Spengler <bspengler@accuvant.com>
2015-02-04 17:33:39 -0500

    Update the message on cleaning -- in a future commit we will look up the proper information in the config and issue this ourselves.  Otherwise actions like searching the django interface will subtly break.

--- cuckoo.py
+++ cuckoo.py
@@ -111,7 +111,7 @@ def cuckoo_clean():
             except (IOError, OSError) as e:
                 log.warning("Error removing file %s: %s", path, e)
 
-    print "Remember to restart mongodb or the django interface will display incorrect data."
+    print "Remember to drop the database from mongodb or the django interface will display incorrect data."
 
 def cuckoo_main(max_analysis_count=0):
     cur_path = os.getcwd()
