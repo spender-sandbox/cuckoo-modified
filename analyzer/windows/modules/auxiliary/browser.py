f3e54a7bd2ed8645665009379342c4ad832a0536
Brad Spengler <bspengler@accuvant.com>
2015-02-06 17:03:57 -0500

    Add a new auxiliary module that starts a browser 30 seconds into the analysis and browses to a URL of your choosing.  To turn on this option, specify "startbrowser=1" in the analysis options.  To specify a URL (so as not to default to yahoo.com), supply it as an argument to the "url=" analysis option.  This provides more functionality than that afforded by the Neverquest package and does it in a way that fits in with Cuckoo's current architecture.

---
+++ analyzer/windows/modules/auxiliary/browser.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2015 Accuvant, Inc. (bspengler@accuvant.com)
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import time
+import os
+import logging
+from threading import Thread
+
+from lib.api.process import Process
+from lib.common.abstracts import Auxiliary
+
+log = logging.getLogger(__name__)
+
+class Browser(Auxiliary, Thread):
+    """Launch a browser 30 seconds into the analysis"""
+
+    def __init__(self, options):
+        Auxiliary.__init__(self, options)
+        Thread.__init__(self)
+        self.do_run = True
+        self.seconds_elapsed = 0
+
+    def stop(self):
+        self.do_run = False
+
+    def run(self):
+        startbrowser = self.options.get("startbrowser")
+        url = self.options.get("url")
+        if not startbrowser:
+            return True
+
+        while self.do_run:
+            time.sleep(1000)
+            self.seconds_elapsed = self.seconds_elapsed + 1
+            if self.seconds_elapsed == 30:
+                iexplore = os.path.join(os.getenv("ProgramFiles"), "Internet Explorer", "iexplore.exe")
+                ie = Process()
+                if not url:
+                    url = "https://www.yahoo.com/"
+                ie.execute(path=iexplore, args="\"" + url + "\"", suspended=False)
+                ie.close()
