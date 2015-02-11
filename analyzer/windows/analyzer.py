dae8f3ba01506c6afd900aec4ed5615d2b75531e
Brad Spengler <bspengler@accuvant.com>
2015-02-11 15:36:12 -0500

    Report 32/64bit process when announcing name

--- analyzer/windows/analyzer.py
+++ analyzer/windows/analyzer.py
@@ -349,9 +349,10 @@ class PipeHandler(Thread):
                                            suspended=suspended)
 
                             filepath = proc.get_filepath()
+                            is_64bit = proc.is_64bit()
                             filename = os.path.basename(filepath)
 
-                            log.info("Announced process name: %s pid: %d", filename, process_id)
+                            log.info("Announced %s process name: %s pid: %d", "64-bit" if is_64bit else "32-bit", filename, process_id)
 
                             if not protected_filename(filename):
                                 res = proc.inject(dll, filepath)
