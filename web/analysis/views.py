ede429dc82af8df9820659e18e65b1297b300682
Brad Spengler <bspengler@accuvant.com>
2015-04-13 15:01:51 -0400

    Add a URL method of obtaining reports, handle errors of arbitrary categories

--- web/analysis/views.py
+++ web/analysis/views.py
@@ -360,19 +360,20 @@ def filereport(request, task_id, category):
         "metadata": "report.metadata.xml",
     }
 
-    file_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(task_id), "reports", formats[category])
-    file_name = str(task_id) + "_" + formats[category]
-    content_type = "application/octet-stream"
+    if category in formats:
+        file_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(task_id), "reports", formats[category])
+        file_name = str(task_id) + "_" + formats[category]
+        content_type = "application/octet-stream"
 
-    if os.path.exists(file_path):
-        response = HttpResponse(open(file_path, "rb").read(), content_type=content_type)
-        response["Content-Disposition"] = "attachment; filename={0}".format(file_name)
+        if os.path.exists(file_path):
+            response = HttpResponse(open(file_path, "rb").read(), content_type=content_type)
+            response["Content-Disposition"] = "attachment; filename={0}".format(file_name)
 
-        return response
-    else:
-        return render_to_response("error.html",
-                                  {"error": "File not found"},
-                                  context_instance=RequestContext(request))
+            return response
+
+    return render_to_response("error.html",
+                              {"error": "File not found"},
+                              context_instance=RequestContext(request))
 
 @require_safe
 def full_memory_dump_file(request, analysis_number):
