1c8d30f983a4fc5f2be8146c72dc11e93033e87a
KillerInstinct <hodges92@hotmail.com>
2015-04-20 08:00:36 -0400

    Add some notes
    (cherry picked from commit 86401f3e1def02a142b3495c88ad7a60e13160aa)

--- modules/feeds/bad_ssl_certs.py
+++ modules/feeds/bad_ssl_certs.py
@@ -5,12 +5,17 @@
 from lib.cuckoo.common.abstracts import Feed
 
 class AbuseCH_SSL(Feed):
+    # Results dict key value / exception handling / logging name
     name = "Bad_SSL_Certs"
 
     def __init__(self):
+        # Location of the feed to be fetched
         self.downloadurl = "https://sslbl.abuse.ch/downloads/ssl_extended.csv"
+        # Used in creating the file path on disk
         self.feedname = "abuse_ch_ssl"
+        # How much time must pass (in hours) before we update
         self.frequency = 6
+        self
 
     def modify(self):
         newdata = ""
@@ -18,11 +23,14 @@ class AbuseCH_SSL(Feed):
         data = self.downloaddata
         for line in data.splitlines():
             item = line.split(",")
+            # Ignore comments
             if len(item) != 6:
                 pass
             else:
-                # Remove header column and deduplicate data
+                # Ignore header column and deduplicate data
                 if "SSL" not in item[4] and item[4] not in seen:
-                    newdata += ",".join(item[4:6]) + "\r\n"
+                    newdata += ",".join(item[4:6]) + "\n"
                 seen.add(item[4])
+        # When we modify download data, we must save this to the self.data
+        # variable instead of self.downloaddata.
         self.data = newdata
