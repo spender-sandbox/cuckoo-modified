e8a39769dd32c78e8de0a3d91ba1a40e4f830ba5
Brad Spengler <bspengler@accuvant.com>
2015-05-06 12:54:06 -0400

    Initial import of some new code to change how logging to the resultserver works

--- analyzer/windows/lib/api/process.py
+++ analyzer/windows/lib/api/process.py
@@ -25,6 +25,7 @@ from lib.common.errors import get_error_string
 from lib.common.rand import random_string
 from lib.common.results import NetlogFile
 from lib.core.config import Config
+from lib.core.log import LogServer, LogHandler
 
 IOCTL_PID = 0x222008
 IOCTL_CUCKOO_PATH = 0x22200C
@@ -68,6 +69,10 @@ class Process:
         self.h_thread = h_thread
         self.suspended = suspended
         self.system_info = SYSTEM_INFO()
+        self.logserver_path = "\\\\.\\PIPE\\" + random_string(8, 12)
+        self.logserver = LogServer(Config("analysis.conf").get_options(), self.logserver_path)
+        self.logserver.daemon = True
+        self.logserver.run()
 
     def __del__(self):
         """Close open handles."""
@@ -530,6 +535,7 @@ class Process:
             config.write("host-ip={0}\n".format(cfg.ip))
             config.write("host-port={0}\n".format(cfg.port))
             config.write("pipe={0}\n".format(PIPE))
+            config.write("logserver={0}\n".format(self.logserver_path))
             config.write("results={0}\n".format(PATHS["root"]))
             config.write("analyzer={0}\n".format(os.getcwd()))
             config.write("first-process={0}\n".format("1" if firstproc else "0"))
---
+++ analyzer/windows/lib/core/log.py
@@ -0,0 +1,132 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation, Accuvant, Inc. (bspengler@accuvant.com)
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import socket
+import logging
+import traceback
+from ctypes import create_string_buffer
+from ctypes import byref, c_int, sizeof
+from threading import Thread
+
+from lib.common.defines import KERNEL32
+from lib.common.defines import ERROR_MORE_DATA, ERROR_PIPE_CONNECTED
+from lib.common.defines import PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE
+from lib.common.defines import PIPE_READMODE_MESSAGE, PIPE_WAIT
+from lib.common.defines import PIPE_UNLIMITED_INSTANCES, INVALID_HANDLE_VALUE
+from lib.core.config import Config
+
+log = logging.getLogger()
+
+BUFSIZE = 512
+LOGBUFSIZE = 16384
+
+class LogHandler(Thread):
+    """Pipe Handler.
+
+    This class handles the notifications received through the Pipe Server and
+    decides what to do with them.
+    """
+
+    def __init__(self, h_pipe, options):
+        """@param h_pipe: PIPE to read.
+           @param options: options for analysis
+        """
+        Thread.__init__(self)
+        self.h_pipe = h_pipe
+        self.resultserver_ip = options.get("host-ip")
+        self.resultserver_port = int(options.get("host-port"))
+        self.resultserver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.resultserver_socket.connect((self.resultserver_ip, self.resultserver_port))
+
+    def run(self):
+        """Run handler.
+        @return: operation status.
+        """
+        try:
+            # Read the data submitted to the Log Server.
+            while True:
+                data = ""
+                while True:
+                    bytes_read = c_int(0)
+                    buf = create_string_buffer(LOGBUFSIZE)
+                    success = KERNEL32.ReadFile(self.h_pipe,
+                                                buf,
+                                                sizeof(buf),
+                                                byref(bytes_read),
+                                                None)
+
+                    data += buf.value
+
+                    if success or KERNEL32.GetLastError() != ERROR_MORE_DATA:
+                        break
+
+                # got an entire message, send it off to the resultserver
+                if data:
+                    self.resultserver_socket.sendall(data)
+
+            KERNEL32.CloseHandle(self.h_pipe)
+            self.resultserver_socket.close()
+
+            return True
+        except Exception as e:
+            error_exc = traceback.format_exc()
+            log.exception(error_exc)
+            return True
+
+class LogServer(Thread):
+    """Cuckoo Log Server.
+
+    This Log Server receives the BSON-encoded logs from cuckoomon loaded in an individual process
+    and forwards them on to the resultserver on the process' behalf, avoiding the need for winsock
+    in cuckoomon and escaping some deadlock issues that can arise from the use of winsock APIs at
+    crucial points in process execution (like the final termination) and also allowing us to again
+    use synchronous logging without side-effects.
+    """
+
+    def __init__(self, options, pipe_name):
+        """@param pipe_name: Cuckoo Log Server PIPE name."""
+        Thread.__init__(self)
+        self.pipe_name = pipe_name
+        self.options = options
+        self.do_run = True
+
+    def stop(self):
+        """Stop Log Server."""
+        self.do_run = False
+
+    def run(self):
+        """Create and run Log Server.
+        @return: operation status.
+        """
+        try:
+            while self.do_run:
+                # Create the Named Pipe.
+                h_pipe = KERNEL32.CreateNamedPipeA(self.pipe_name,
+                                                   PIPE_ACCESS_INBOUND,
+                                                   PIPE_TYPE_MESSAGE |
+                                                   PIPE_READMODE_MESSAGE |
+                                                   PIPE_WAIT,
+                                                   PIPE_UNLIMITED_INSTANCES,
+                                                   BUFSIZE,
+                                                   LOGBUFSIZE,
+                                                   0,
+                                                   None)
+
+                if h_pipe == INVALID_HANDLE_VALUE:
+                    return False
+
+                # If we receive a connection to the pipe, we invoke the handler.
+                if KERNEL32.ConnectNamedPipe(h_pipe, None) or KERNEL32.GetLastError() == ERROR_PIPE_CONNECTED:
+                    handler = LogHandler(h_pipe, self.options)
+                    handler.daemon = True
+                    handler.start()
+                else:
+                    KERNEL32.CloseHandle(h_pipe)
+
+            return True
+        except Exception as e:
+            error_exc = traceback.format_exc()
+            log.exception(error_exc)
+            return True
+
