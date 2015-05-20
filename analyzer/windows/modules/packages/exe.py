2c8faa1eaf7cf466b531890c76bc54c80b4a9cec
Brad Spengler <bspengler@accuvant.com>
2015-05-20 13:58:07 -0400

    Force executables to have an extension due to some MFC code that will crash the app if one is not found

--- analyzer/windows/modules/packages/exe.py
+++ analyzer/windows/modules/packages/exe.py
@@ -14,6 +14,16 @@ class Exe(Package):
         args = self.options.get("arguments")
         appdata = self.options.get("appdata")
         runasx86 = self.options.get("runasx86")
+
+        # If the file doesn't have an extension, add .exe
+        # See CWinApp::SetCurrentHandles(), it will throw
+        # an exception that will crash the app if it does
+        # not find an extension on the main exe's filename
+        if "." not in os.path.basename(path):
+            new_path = path + ".exe"
+            os.rename(path, new_path)
+            path = new_path
+
         if appdata:
             # run the executable from the APPDATA directory, required for some malware
             basepath = os.getenv('APPDATA')
