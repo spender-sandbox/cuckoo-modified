039685489ac0cbe5e571b436bbc334d8f73aa7b8
rep <ms@mwcollect.org>
2015-05-22 17:30:18 +0200

    handle fds better
    (cherry picked from commit e2b33dd881b9dd9a08b2f511db1021358989d653)
    
    Conflicts:
    	lib/cuckoo/core/resultserver.py

--- lib/cuckoo/core/resultserver.py
+++ lib/cuckoo/core/resultserver.py
@@ -138,6 +138,13 @@ class ResultHandler(SocketServer.BaseRequestHandler):
     def finish(self):
         self.done_event.set()
 
+        if self.protocol:
+            self.protocol.close()
+        if self.logfd:
+            self.logfd.close()
+        if self.rawlogfd:
+            self.rawlogfd.close()
+
     def wait_sock_or_end(self):
         while True:
             if self.end_request.isSet():
@@ -222,13 +229,6 @@ class ResultHandler(SocketServer.BaseRequestHandler):
             log.exception("FIXME - exception in resultserver connection %s",
                           str(self.client_address))
 
-        if self.protocol:
-            self.protocol.close()
-
-        if self.logfd:
-            self.logfd.close()
-        if self.rawlogfd:
-            self.rawlogfd.close()
 
         log.debug("Connection closed: {0}:{1}".format(ip, port))
 
