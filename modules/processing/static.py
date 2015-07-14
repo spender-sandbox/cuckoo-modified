02979b06887238f476eb866a7b6de08c1deb3e50
Brad Spengler <bspengler@accuvant.com>
2015-07-14 11:17:52 -0400

    Extract PDB path from binaries and report on the static analysis pages

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -110,6 +110,27 @@ class PortableExecutable:
         except:
             return None
 
+    def _get_pdb_path(self):
+        if not self.pe:
+            return None
+
+        try:
+            for dbg in self.pe.DIRECTORY_ENTRY_DEBUG:
+                dbgst = dbg.struct
+                dbgdata = self.pe.__data__[dbgst.PointerToRawData:dbgst.PointerToRawData+dbgst.SizeOfData]
+                if dbgst.Type == 4: #MISC
+                    datatype, length, uniflag = struct.unpack_from("IIB", dbgdata)
+                    return str(dbgdata[12:length]).rstrip('\0')
+                elif dbgst.Type == 2: #CODEVIEW
+                    if dbgdata[:4] == "RSDS":
+                        return str(dbgdata[24:]).rstrip('\0')
+                    elif dbgdata[:4] == "NB10":
+                        return str(dbgdata[16:]).rstrip('\0')
+        except:
+            pass
+
+        return None
+
     def _get_imported_symbols(self):
         """Gets imported symbols.
         @return: imported symbols dict or None.
@@ -468,6 +489,7 @@ class PortableExecutable:
         results["pe_imagebase"] = self._get_imagebase()
         results["pe_entrypoint"] = self._get_entrypoint()
         results["pe_osversion"] = self._get_osversion()
+        results["pe_pdbpath"] = self._get_pdb_path()
         results["pe_imports"] = self._get_imported_symbols()
         results["pe_exports"] = self._get_exported_symbols()
         results["pe_dirents"] = self._get_directory_entries()
