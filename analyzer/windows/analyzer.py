c6082d77572c6d2a015284eebe428a44dfa792c2
Brad Spengler <bspengler@accuvant.com>
2015-07-15 16:22:33 -0400

    It was observed a while ago that Cuckoo handles alternate streams improperly for the purposes of dumping deleted files.  Malware families like Poweliks actually abuse this, by creating and executing a binary in the :0 stream, then deleting the original file which the streams are attached to.  Cuckoo would only dump the main file and then attempt at the end of analysis to dump the remaining stream file still in its dump list, and find at that point that it doesn't exist (because the file it was attached to was already removed).  Fix this by making move handling aware of streams and make the delete handling dump all associated streams for a file.  We'll also no longer bother dumping existing files that were deleted to reduce noise -- we'll only dump deleted files that we had some previous interest in: actual dropped files.

--- analyzer/windows/analyzer.py
+++ analyzer/windows/analyzer.py
@@ -144,15 +144,27 @@ def dump_file(file_path):
 
 
 def del_file(fname):
-    dump_file(fname)
+
+    deleted_idxes = []
 
     # Filenames are case-insensitive in windows.
-    fnames = [x.lower() for x in FILES_LIST]
+    fnamelower = fname.lower()
+
+    # we only dump files during deletion that we were previously aware of
+    for idx, name in FILES_LIST:
+        namelower = name.lower()
+        # dump streams associated with the file too
+        if namelower == fnamelower or (namelower.startswith(fnamelower) and namelower[len(fnamelower)] == ':'):
+            dump_file(name)
+            deleted_idxes.append(idx)
+
+    for name in dumped_files:
+        dump_file(name)
 
     # If this filename exists in the FILES_LIST, then delete it, because it
     # doesn't exist anymore anyway.
-    if fname.lower() in fnames:
-        FILES_LIST.pop(fnames.index(fname.lower()))
+    for idx in deleted_idxes:
+        FILES_LIST.pop(idx)
 
 def move_file(old_fname, new_fname):
     # Filenames are case-insensitive in windows.
@@ -177,6 +189,9 @@ def move_file(old_fname, new_fname):
                replacepath = new_fname
            else:
                replacepath = new_fname + u"\\"
+        elif fname.startswith(lower_old_fname + u":"):
+            matchpath = lower_old_fname + u":"
+            replacepath = new_fname + u":"
 
         if matchpath:
             # Replace the old filename by the new filename, or replace the subdirectory if moved
