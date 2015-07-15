5b6b0283369011e475e8994e8d0c2335f52da077
Brad Spengler <bspengler@accuvant.com>
2015-07-14 20:10:00 -0400

    Add support for submitting Trend Micro quarantine files directly.  The decoding method is the first publicly published for this format, reversed and developed by Accuvant, Inc.

--- lib/cuckoo/common/quarantine.py
+++ lib/cuckoo/common/quarantine.py
@@ -3,6 +3,9 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import os
+import struct
+from binascii import crc32
+
 import lib.cuckoo.common.office.olefile as olefile
 from lib.cuckoo.common.utils import store_temp_file
 
@@ -10,6 +13,84 @@ def bytearray_xor(data, key):
     for i in range(len(data)):
         data[i] ^= key
     return data
+
+def read_trend_tag(data, offset):
+    """ @return a code byte and data tuple
+    """
+    code, length = struct.unpack("<BH", data[offset:offset+3])
+    return code, bytes(data[offset+3:offset+3+length])
+
+# Never before published, reversed & developed by Accuvant, Inc.
+# We don't need most of the header fields but include them here
+# for the sake of documentation
+
+def trend_unquarantine(file):
+    with open(file, "rb") as quarfile:
+        qdata = quarfile.read()
+
+    data = bytearray_xor(bytearray(qdata), 0xff)
+
+    magic, dataoffset, numtags = struct.unpack("<IIH", data[:10])
+    if magic != 0x58425356: # VSBX
+        return None
+    origpath = "C:\\"
+    origname = "UnknownTrendFile.bin"
+    platform = "Unknown"
+    attributes = 0x00000000
+    unknownval = 0
+    basekey = 0x00000000
+    encmethod = 0
+
+    if numtags > 15:
+        return None
+
+    dataoffset += 10
+    offset = 10
+    for i in range(numtags):
+        code, tagdata = read_trend_tag(data, offset)
+        if code == 1: # original pathname
+            origpath = unicode(tagdata, encoding="utf16")
+        elif code == 2: # original filename
+            origname = unicode(tagdata, encoding="utf16")
+        elif code == 3: # platform
+            platform = str(tagdata)
+        elif code == 4: # file attributes
+            attributes = struct.unpack("<I", tagdata)[0]
+        elif code == 5: # unknown, generally 1
+            unknownval = struct.unpack("<I", tagdata)[0]
+        elif code == 6: # base key
+            basekey = struct.unpack("<I", tagdata)[0]
+        elif code == 7: # encryption method: 1 == xor FF, 2 = CRC method
+            encmethod = struct.unpack("<I", tagdata)[0]
+        offset += 3 + len(tagdata)
+
+    if encmethod != 2:
+        return store_temp_file(data[dataoffset:len(data)], origname)
+
+    bytesleft = len(data) - dataoffset
+    unaligned = bytesleft % 4
+    firstiter = 1
+    curoffset = dataoffset
+    while bytesleft:
+        off = curoffset
+        if firstiter:
+            off = curoffset - unaligned
+        keyval = basekey + off
+        buf = struct.pack("<I", keyval)
+        crc = crc32(buf) & 0xffffffff
+        crcbuf = bytearray(struct.pack("<I", crc))
+
+        for i in range(unaligned, 4):
+            if not bytesleft:
+                break
+            data[curoffset] ^= crcbuf[i]
+            curoffset += 1
+            bytesleft -= 1
+
+        unaligned = 0
+        firstiter = 0
+
+    return store_temp_file(data[dataoffset:len(data)], origname)
 
 def mcafee_unquarantine(file):
     if not olefile.isOleFile(file):
@@ -70,5 +151,9 @@ def unquarantine(file):
 
     if ext.lower() == ".bup" or olefile.isOleFile(file):
         return mcafee_unquarantine(file)
+
+    quarfile = trend_unquarantine(file)
+    if quarfile:
+        return quarfile
 
     return forefront_unquarantine(file)
