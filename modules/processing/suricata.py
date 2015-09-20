d193639295ac45366071fc2a0b0072a5f4d8ea3c
Sean Whalen <whalenster@gmail.com>
2015-09-20 18:01:24 -0400

    Skip log lines that are missing an id

--- modules/processing/suricata.py
+++ modules/processing/suricata.py
@@ -224,6 +224,9 @@ class Suricata(Processing):
                 except:
                     log.warning("failed to load JSON from file log")
                     continue
+                # Some log entries do not have an id
+                if "id" not in d:
+                    continue
                 src_file = "%s/file.%s" % (SURICATA_FILES_DIR_FULL_PATH,d["id"])
                 if os.path.exists(src_file):
                     if SURICATA_FILE_COPY_MAGIC_RE and SURICATA_FILE_COPY_DST_DIR and os.path.exists(SURICATA_FILE_COPY_DST_DIR):
