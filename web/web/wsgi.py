b93d2d23cd47a491afd135f4ad588426954c929b
geudrik <litke.p@arcti.cc>
2015-09-23 11:19:49 -0400

    Adding clarity to the WSGI file for those that want to run the webUI with Apache2/HTTPS/WSGI

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -19,6 +19,51 @@ framework.
 """
 import os
 
+"""
+
+:: Correctly setting up WSGI w/ Apache2, using only HTTPS. Start here.
+
+You can use the following command to generate SSL certs for an HTTPS setup.
+# sudo openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout \ 
+    /etc/apache2/ssl/cert.key -out /etc/apache2/ssl/cert.crt
+
+The following Apache2 vhost will work plug-and-play with the above command
+// Begin Apache2 config for WSGI usage
+
+<VirtualHost *:443>
+
+        # Remember to change paths where necessary
+
+        SSLEngine On
+        SSLCertificateFile      /etc/apache2/ssl/cert.crt
+        SSLCertificateKeyFile   /etc/apache2/ssl/cert.key
+
+        WSGIDaemonProcess web processes=5 threads=20
+
+        WSGIScriptAlias         /       /opt/cuckoo/cuckoo-modified/web/web/wsgi.py
+
+        <Directory /opt/cuckoo/cuckoo-modified/web>
+                Require         all     granted
+                WSGIScriptReloading On
+        </Directory>
+
+        Alias /static /opt/cuckoo/cuckoo-modified/web/static
+
+        ErrorLog        ${APACHE_LOG_DIR}/error.log
+        LogLevel        error
+        CustomLog       ${APACHE_LOG_DIR}/access.log    combined
+
+</VirtualHost>
+
+// End Apache2 config for WSGI usage
+
+Uncomment and edit the following lines to match your install location
+"""
+#import sys
+#sys.path.append('/path/to/cuckoo-modified')
+#sys.path.append('/path/to/cuckoo-modified/web')
+#os.chdir('/path/to/cuckoo-modified/web')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 # This application object is used by any WSGI server configured to use this
