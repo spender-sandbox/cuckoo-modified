e01f54532a536420a9e64c8e84bb56141daee3eb
KillerInstinct <hodges92@hotmail.com>
2015-12-09 06:10:45 -0500

    Add a reporting module to manage data retention A decent portion of the code (time searching for Task IDs) and similar code came from https://github.com/wmetcalf/cuckoo-modified/commit/16ce8331d1d747242dd9c1cd2cc667ec63f8eb03 however I further extended this functionality by adding the ability to query for tasks < or >. Now we can query on date for neatness as with most other retention types while only querying for tasks which have not already had data deleted. The reporting module keeps track of already touched tasks per retention category via a file in storage/retention.

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -1064,7 +1064,8 @@ class Database(object):
     @classlock
     def list_tasks(self, limit=None, details=False, category=None,
                    offset=None, status=None, sample_id=None, not_status=None,
-                   completed_after=None, order_by=None):
+                   completed_after=None, order_by=None, added_before=None,
+                   id_before=None, id_after=None):
         """Retrieve list of task.
         @param limit: specify a limit of entries.
         @param details: if details about must be included
@@ -1075,6 +1076,9 @@ class Database(object):
         @param not_status: exclude this task status from filter
         @param completed_after: only list tasks completed after this timestamp
         @param order_by: definition which field to sort by
+        @param added_before: tasks added before a specific timestamp
+        @param id_before: filter by tasks which is less than this value
+        @param id_after filter by tasks which is greater than this value
         @return: list of tasks.
         """
         session = self.Session()
@@ -1091,9 +1095,14 @@ class Database(object):
                 search = search.options(joinedload("guest"), joinedload("errors"), joinedload("tags"))
             if sample_id is not None:
                 search = search.filter_by(sample_id=sample_id)
+            if id_before is not None:
+                search = search.filter(Task.id < id_before)
+            if id_after is not None:
+                search = search.filter(Task.id > id_after)
             if completed_after:
                 search = search.filter(Task.completed_on > completed_after)
-
+            if added_before:
+                search = search.filter(Task.added_on < added_before)
             if order_by is not None:
                 search = search.order_by(order_by)
             else:
