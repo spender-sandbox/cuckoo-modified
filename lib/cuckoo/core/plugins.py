12879a05fc2e1fdc7bba7a9f598439d778610c97
Brad Spengler <spender+sandbox@gmail.com>
2016-03-15 12:14:08 -0400

    Update index for grabbing ClamAV name

--- lib/cuckoo/core/plugins.py
+++ lib/cuckoo/core/plugins.py
@@ -567,7 +567,7 @@ class RunSignatures(object):
         # fall back to ClamAV detection
         if not family and self.results["info"]["category"] == "file" and "clamav" in self.results["target"]["file"] and self.results["target"]["file"]["clamav"] and self.results["target"]["file"]["clamav"].startswith("Win.Trojan."):
             words = re.findall(r"[A-Za-z0-9]+", self.results["target"]["file"]["clamav"])
-            family = words[3]
+            family = words[2]
 
         self.results["malfamily"] = family
 
