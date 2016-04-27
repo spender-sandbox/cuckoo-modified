3ef18e1842ce395cdda9c740de770d0f1fe114dc
Brad Spengler <spender+sandbox@gmail.com>
2016-04-27 14:23:44 -0400

    Handle process tree iteratively rather than recursively

--- web/analysis/templatetags/generic_tags.py
+++ web/analysis/templatetags/generic_tags.py
@@ -1,5 +1,22 @@
 from django.template.defaultfilters import register
+from collections import deque
 
 @register.filter("endswith")
 def endswith(value, thestr):
     return value.endswith(thestr)
+
+@register.filter("proctreetolist")
+def proctreetolist(tree):
+    stack = deque(tree)
+    outlist = []
+    while stack:
+        node = stack.popleft()
+        outlist.append(node)
+        if "startchildren" in node or "endchildren" in node:
+            continue
+        if node["children"]:
+            stack.appendleft({"endchildren" : 1})
+            stack.extendleft(reversed(node["children"]))
+            stack.appendleft({"startchildren" : 1})
+    return outlist
+    
