55ca5c87d92158be90e52b1211fe475b654fca7b
Brad Spengler <spender+sandbox@gmail.com>
2016-06-06 11:40:38 -0400

    Fix timeout calculation in guest -- assuming a 1s sleep corresponds to 1s of walltime execution is wrong, old upstream cuckoo bug.  Perform the check the same as the host doing critical timeout detection correctly does it

--- analyzer/windows/analyzer.py
+++ analyzer/windows/analyzer.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import time
 import socket
 import struct
 import random
@@ -877,6 +878,7 @@ class Analyzer:
         @return: operation status.
         """
         self.prepare()
+        timeout_time = time.time() + int(self.config.timeout)
 
         log.debug("Starting analyzer from: %s", os.getcwd())
         log.debug("Storing results at: %s", PATHS["root"])
@@ -999,7 +1001,6 @@ class Analyzer:
             log.info("Enabled timeout enforce, running for the full timeout.")
             pid_check = False
 
-        time_counter = 0
         kernel_analysis = self.config.get_options().get("kernel_analysis", False)
 
         if kernel_analysis != False:
@@ -1008,8 +1009,7 @@ class Analyzer:
         emptytime = None
 
         while True:
-            time_counter += 1
-            if time_counter == int(self.config.timeout):
+            if time.time() > timeout_time:
                 log.info("Analysis timeout hit, terminating analysis.")
                 break
 
