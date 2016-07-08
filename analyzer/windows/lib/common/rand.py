aac72181b343b5ac3e9e4317e1865a693cdf4fd3
KillerInstinct <hodges92@hotmail.com>
2016-07-08 11:36:24 -0400

    Defeat macro MRU-based antisandbox checks Added randomized MRUs to office softwares if they are installed. Also improved the previous random_string function to allow specific character sets.

--- analyzer/windows/lib/common/rand.py
+++ analyzer/windows/lib/common/rand.py
@@ -1,12 +1,15 @@
 import random
 import string
 
-def random_string(minimum, maximum=None):
+def random_string(minimum, maximum=None, charset=None):
     if maximum is None:
         maximum = minimum
 
     count = random.randint(minimum, maximum)
-    return "".join(random.choice(string.ascii_letters) for x in xrange(count))
+    if not charset:
+        return "".join(random.choice(string.ascii_letters) for x in xrange(count))
+
+    return ''.join(random.choice(charset) for x in range(count))
 
 def random_integer(digits):
     start = 10 ** (digits - 1)
