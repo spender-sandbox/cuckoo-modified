4e98efcf07ea6f69f1042434a3195cc0a8eed93a
SeanKim777 <ilrud@hotmail.com>
2016-07-22 16:36:01 +1000

    To handle url without '/' at the end in conf file

--- modules/reporting/misp.py
+++ modules/reporting/misp.py
@@ -177,7 +177,7 @@ class MISP(Report):
                                     tmp_misp[eid].setdefault("iocs", list())
                                 tmp_misp[eid]["iocs"].append(ioc)
                                 tmp_misp[eid].setdefault("eid", eid)
-                                tmp_misp[eid].setdefault("url", url+"events/view/")
+                                tmp_misp[eid].setdefault("url", os.path.join(url, "events/view/"))
                                 tmp_misp[eid].setdefault("date", date)
                                 tmp_misp[eid].setdefault("level", event.get("threat_level_id",""))
                                 tmp_misp[eid].setdefault("info", event.get("info", "").strip())
